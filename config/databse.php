c04e9cabe4f43e26cb84069a4633e61398475d98
Github-arrow <brian.d.pelarez@outlook.ph>
2024-03-08 13:30:55 +0800

    initial commit

---
+++ config/databse.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database_name = "db_hms";
+
+$conn = new mysqli($servername, $username, $password, $database_name);
+
+if ($conn->connect_error){
+    die("Conection Failed: " . $conn->connect_error);
+}
+echo "Connected Successfully";
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Individual Activity</title>
+</head>
+
+<body>
+    <Strong>Search :</Strong>
+    <input type="text" placeholder="Search here...">
+
+    <hr>
+
+    <thead>
+        <tr>
+            <th>Guest ID</th>
+            <th>First Name</th>
+        </tr>
+    </thead>
+    <tbody id="results"></tbody>
+
+</body>
+
+</html>
